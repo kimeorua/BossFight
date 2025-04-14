dab4c9957a7092eed34bf7fbc78202053ea2255b
kimeorua <kimeorua1222@naver.com>
2025-04-14 14:37:08 +0900

    콤보 공격 Tag 추가

--- Source/BossFight/Private/BFGameplayTags.cpp
+++ Source/BossFight/Private/BFGameplayTags.cpp
@@ -16,4 +16,17 @@ namespace BFGameplayTag
 
 	// Player Tags
 	UE_DEFINE_GAMEPLAY_TAG(Player_WeaponEquip, "Player.WeaponEquip");
+
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Combo, "Player.Ability.Combo");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Combo_1, "Player.Ability.Combo.1");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Combo_2, "Player.Ability.Combo.2");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Combo_3, "Player.Ability.Combo.3");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Combo_4, "Player.Ability.Combo.4");
+
+	UE_DEFINE_GAMEPLAY_TAG(Player_Status_ReadyCombo2, "Player.Status.ReadyCombo2");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Status_ReadyCombo3, "Player.Status.ReadyCombo3");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Status_ReadyCombo4, "Player.Status.ReadyCombo4");
+
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_ReadyCombo, "Player.Event.ReadyCombo");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_EndCombo, "Player.Event.EndCombo");
 }
